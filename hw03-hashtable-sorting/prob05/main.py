e0b6abaf143b76136ebbb1ead106824847015eb1
Joseph Stowers <joe_s_nm@yahoo.com>
2023-09-24 14:44:18 -0500

    complete naive O(n^2) solution

---
+++ hw03-hashtable-sorting/prob05/main.py
@@ -0,0 +1,63 @@
+# HW 3
+# Problem No. 5
+
+# Longest Subarray Length
+# -----------------------------
+# Given an array of integers and a target value, write a function that 
+# returns the length of the longest subarray with a sum equal to the 
+# target value.
+
+# Note: while the sliding window technique is acceptable as a solution, 
+# try solving this using a hash table.
+
+# Example
+# -----------------
+# array = [3, 1, -1, 2, -1, 5, -2, 3]
+# target = 3
+# result = 5 => [-1, 2, -1, 5, -2]
+
+
+array = [3, 1, -1, 2, -1, 5, -2, 3]
+
+target = 3
+
+def longest_subarray(array, target):
+    ht = {}
+
+    for i, num in enumerate(array):
+        sum = num
+        length = 1
+
+        if (sum == target and ht.get(sum) == None):
+            ht.update({sum: length})
+
+        pointer = i+1
+
+        while (pointer < len(array)):
+            sum += array[pointer]
+            length += 1
+
+            print("i =", i, " pointer =", pointer, " sum =", sum, " length =", length)
+
+            if sum  == target and length > ht.get(sum):
+                print("ht.get(sum) =", ht.get(sum))
+                ht.update({sum: length})
+            pointer += 1
+        print("ht=", ht)
+
+    return ht[target]
+
+
+result = longest_subarray(array, target)
+print("result =", result)
+
+
+# list = [
+#     {[3, 1, -1, 2, -1, 5, -2, 3], 3},
+# ]
+
+# for index, array in enumerate(list):
+#     missingValues = find_missing_numbers(array[i].)
+#     print("--------------------------------------------------------------")
+#     print(f'{index + 1}. {str(array):25} missing values = {missingValues}')  
+
