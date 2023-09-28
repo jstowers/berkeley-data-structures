705e58d995087117f264695021f59e8fe0197b77
Joseph Stowers <joe_s_nm@yahoo.com>
2023-09-27 23:56:42 -0500

    add HW4 folder

---
+++ hw04-linkedlist-tree/prob01/main.py
@@ -0,0 +1,9 @@
+# HW 4
+# Problem No. 1
+
+# Reverse Doubly Linked List
+# --------------------------
+# Write a function to reverse the elements in a doubly linked list. 
+# Do not simply print it out. It must have the references correctly set in reversed order.
+# Also, write a print method – this should help with debugging.
+
